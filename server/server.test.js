adb62e52f09c656c4fdf50296b9f343e1282d18a
PiotrBerebecki <petebere@gmail.com>
2016-12-30 10:25:12 +0700

    Add describe blocks

--- server/server.test.js
+++ server/server.test.js
@@ -4,50 +4,52 @@ const expect = require('expect');
 const app = require('./server').app;
 
 
-// TEST 1
-it('should return the correct response body for root route', (done) => {
-  request(app)
-    .get('/')
-    .expect('Hello world!')
-    .end(done);  
-});
+describe('Express', () => {
+  // TEST 1
+  it('should return the correct response body for root route', (done) => {
+    request(app)
+      .get('/')
+      .expect('Hello world!')
+      .end(done);  
+  });
 
 
-// TEST 2
-it('should return the correct response for wrong url', (done) => {
-  request(app)
-    .get('/bad')
-    .expect(404)
-    .expect({
-      error: 'Not found'
-    })
-    .end(done);
-});
+  // TEST 2
+  it('should return the correct response for wrong url', (done) => {
+    request(app)
+      .get('/bad')
+      .expect(404)
+      .expect({
+        error: 'Not found'
+      })
+      .end(done);
+  });
 
 
-// TEST 3
-it('should include a relevant piece of info for wrong url', (done) => {
-  request(app)
-    .get('/projects')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Mocha playground app'
-      });
-    })
-    .end(done);  
-});
+  // TEST 3
+  it('should include a relevant piece of info for wrong url', (done) => {
+    request(app)
+      .get('/projects')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          name: 'Mocha playground app'
+        });
+      })
+      .end(done);  
+  });
 
 
-// TEST 4
-it('should return the correct response for users route', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Pete', age: 25
-      });
-    })
-    .end(done);
+  // TEST 4
+  it('should return the correct response for users route', (done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          name: 'Pete', age: 25
+        });
+      })
+      .end(done);
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,80 +3,89 @@ const expect = require('expect');
 const utils = require('./utils');
 
 
-it('should add two numbers', () => {
-  const res = utils.add(33, 11);
+describe('Utils', () => {
   
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44 but got ${res}`);
-  // } else {
-  //   console.log(`Expected 44 and got ${res}`);
-  // }
-  
-  // expect(res).toBeA('number');
-  expect(res).toBe(44).toBeA('number');
-});
-
-
-it('should add two numbers in async function', (done) => {
-  utils.asyncAdd(3, 4, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+  describe('#sync test', () => {
+    it('should add two numbers', () => {
+      const res = utils.add(33, 11);
+      
+      // if (res !== 44) {
+      //   throw new Error(`Expected 44 but got ${res}`);
+      // } else {
+      //   console.log(`Expected 44 and got ${res}`);
+      // }
+      
+      expect(res).toBe(44).toBeA('number');
+    });
+    
+    
+    it('should square a number', () => {
+      const res = utils.square(8);
+      
+      // if (res !== 64) {
+      //   throw new Error(`Expected 64 but got ${res}`);
+      // } else {
+      //   console.log(`Expected 64 and got ${res}`);
+      // }
+      
+      expect(res).toBe(64).toBeA('number');
+    });    
   });
-});
-
-
-it('should square a number', () => {
-  const res = utils.square(8);
   
-  // if (res !== 64) {
-  //   throw new Error(`Expected 64 but got ${res}`);
-  // } else {
-  //   console.log(`Expected 64 and got ${res}`);
-  // }
-  
-  expect(res).toBe(64).toBeA('number');
-});
-
-it('should square a number in async function', (done) => {
-  utils.asyncSquare(4, (square) => {
-    expect(square).toBe(16).toBeA('number');
-    done();
+  describe('#async tests', () => {
+    it('should add two numbers in async function', (done) => {
+      utils.asyncAdd(3, 4, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
+    
+    
+    it('should square a number in async function', (done) => {
+      utils.asyncSquare(4, (square) => {
+        expect(square).toBe(16).toBeA('number');
+        done();
+      });
+    });
   });
+  
 });
 
 
-it('should expect some value', () => {
-  expect(12).toNotBe(13);
-  
-  // compare objects
-  // to be uses triple equals so we can't use it for objects
-  // expect({name: 'Piotr'}).toBe({name: 'Piotr'});
-  
-  // instead we use toEqual
-  expect({name: 'Piotr'}).toEqual({name: 'Piotr'}); // pass
-  // expect({name: 'Piotr'}).toEqual({name: 'piotr'}); // not pass
-  
-  // check if array includes
-  expect([1,2,3]).toInclude(2);
-  expect([1,2,3]).toExclude(4);
-  
-  expect({name: 'Piotr'}).toInclude({name: 'Piotr'});
-});
+describe('Learning tests', () => {
+  it('should expect some value', () => {
+    expect(12).toNotBe(13);
+    
+    // compare objects
+    // to be uses triple equals so we can't use it for objects
+    // expect({name: 'Piotr'}).toBe({name: 'Piotr'});
+    
+    // instead we use toEqual
+    expect({name: 'Piotr'}).toEqual({name: 'Piotr'}); // pass
+    // expect({name: 'Piotr'}).toEqual({name: 'piotr'}); // not pass
+    
+    // check if array includes
+    expect([1,2,3]).toInclude(2);
+    expect([1,2,3]).toExclude(4);
+    
+    expect({name: 'Piotr'}).toInclude({name: 'Piotr'});
+  });
 
 
-it('should set firstName and lastName', () => {
-  const user1 = {
-    age: 25,
-    location: 'London'
-  };
-  
-  const fullName = 'Peter Bradley';
-  
-  const user1Update = utils.setName(user1, fullName);
-  
-  expect(user1Update).toBeA('object');
-  expect(user1Update).toInclude({
-    firstName: 'Peter',
-    lastName: 'Bradley'
+  it('should set firstName and lastName', () => {
+    const user1 = {
+      age: 25,
+      location: 'London'
+    };
+    
+    const fullName = 'Peter Bradley';
+    
+    const user1Update = utils.setName(user1, fullName);
+    
+    expect(user1Update).toBeA('object');
+    expect(user1Update).toInclude({
+      firstName: 'Peter',
+      lastName: 'Bradley'
+    });
   });
 });
